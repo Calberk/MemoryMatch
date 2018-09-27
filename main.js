9cd045136d064acfb2f93360e66bbcf3a7c8c28d
Calberk <park.edmund@gmail.com>
2018-09-27 14:19:23 -0700

    Memory Match version0.5 - Edmund Park

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-$("document").ready(initializeApp()
+$(document).ready(initializeApp);
 
-var images = ["images/logo.jpg"];
 
 function initializeApp(){
-    pageSetup();
+    applyHandler();
 }
 
-function pageSetup(){
-var logo = $(".logo").css("background-image", "url(" + images[0] +")");
-$("body").append(logo);
+var firstSelectedCard = null;
+var secondSelectedCard = null;
+var total_possible_matches = 2;
+var match_counter = 0;
+
+function applyHandler() {
+    $(".card").click(cardClick)
+}
+
+function cardClick(){
+    var firstCardChecked = $(firstSelectedCard).find(".front > img").attr("src")
+    var secondCardChecked = $(firstSelectedCard).find(".front > img").attr("src")
+    if(firstSelectedCard === null){
+        firstSelectedCard = $(event.currentTarget);
+        firstSelectedCard.addClass("hide");
+    }else {
+        secondSelectedCard = event.currentTarget;
+        hideCard(event.currentTarget);
+        if (firstCardChecked === secondCardChecked){
+            console.log("these are the same")
+        }else{
+            console.log("not the same");
+        }
+    }
+}
+
+function hideCard(card){
+        $(event.currentTarget).addClass('hide');
 }
